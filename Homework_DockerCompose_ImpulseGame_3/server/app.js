93a18aa4a322b67951d9b0b3185bb91d2f550744
Sergey Popkov <serega1986-16@mail.ru>
2022-12-28 03:18:01 +0300

    Доработки

--- Homework_DockerCompose_ImpulseGame_3/server/app.js
+++ Homework_DockerCompose_ImpulseGame_3/server/app.js
@@ -8,6 +8,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+var cors = require('cors');
+app.use(cors())
+
+app.all(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+});
 
 require('./controllers/authController')(app)
 require('./controllers/accountController')(app)
